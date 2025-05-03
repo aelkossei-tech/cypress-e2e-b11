d968ec21a00309607f94a7883f4c12a16884ecd0
aelkossei-tech <ayahelkossie@gmail.com>
2025-05-03 14:05:19 -0500

    modified 09-dropdrown

--- cypress/e2e/integration/09-dropdown-select.cy.js
+++ cypress/e2e/integration/09-dropdown-select.cy.js
@@ -2,38 +2,51 @@
 
 describe('Dropdown select', () => {
     beforeEach(() => {
-        cy.contains(".card", "Dropdowns").click();
-      });
-
-    it('Select product & color', () => {
-        //cy.get('#product_dropdown').select(2); // select() --> could either use the index of the item in the menu OR visible text (more preferred)
-        // visible text should exactly match (case-sensitive)
-
-      //  cy.get('#color_dropdown')
-       // .select('Yellow')
-        
-
-        cy.get('#product_dropdown').select('Apple Watch Series 8'); 
-        cy.get('#color_dropdown').select('Yellow'); 
-
-        cy.get('product_dropdown option:selected').should('have.text', 'Apple Watch Series 8'); 
-        cy.get('color_dropdown option:selected').should('have.text', 'Yellow'); 
-    })
-
-    it.only('Select product & color & delivery', () => {
-        cy.get('product_dropdown option:selected').should('have.text', 'Select...');
-        cy.get('product_dropdown').select('Apple Watch Series 8'); 
-        cy.get('product_dropdown option:selected').select('Apple Watch Series 8'); 
+      cy.contains(".card", "Dropdowns").click();
     });
+  
+    it('Select product & color', () => {
+      // cy.get('#product_dropdown')
+      //   .select('Apple Watch Series 8')
+      //   .should('have.value', 'Apple Watch Series 8');
+  
+      // cy.get('#color_dropdown')
+      //   .select('Yellow')
+      //   .should('have.value', 'Yellow');
 
+      //cy.get('#product_dropdown').select(2); // select() --> could either use the index of the item in the menu OR visible text (more preferred)
+      // visible text should exactly match (case-sensitive)
+
+
+  
+      cy.get('#product_dropdown').select('Apple Watch Series 8');
+      cy.get('#color_dropdown').select('Yellow');
+  
+      cy.get('#product_dropdown option:selected').should('have.text', 'Apple Watch Series 8');
+      cy.get('#color_dropdown option:selected').should('have.text', 'Yellow');
+    })
+  
+    it.only('Select product & color & delivery', () => {
+      cy.get('#product_dropdown option:selected').should('contain.text', 'Select');
+      cy.get('#product_dropdown').select('Apple Watch Series 8');
+      cy.get('#product_dropdown option:selected').should('have.text', 'Apple Watch Series 8');
+  
+      cy.get('#color_dropdown option:selected').should('contain.text', 'Select');
+      cy.get('#color_dropdown').select('Yellow');
+      cy.get('#color_dropdown').select('Silver');
+      cy.get('#color_dropdown option:selected').should('have.text', 'Silver');
+  
+      cy.get('#shipment_dropdown').click();
+      cy.get('span[aria-label="Delivery"]').click();
+    });
+  
     it('Validate the result of selections', () => {
-        /*
+      /*
         Select "iPad Pro 11"
         Select "Green"
-        Select "Pickup"
-        Then click on "Submit" button
-        You should your result under "Result". 
-        Validate "Your Green iPad Pro 11 is ready to be picked up."
-        */
+        Select "Pick up"
+        Click on "SUBMIT" button
+        Validate "Your Green iPad Pro 11 is ready to be picked up." is visible
+      */
     })
-})
+  })
