cd4f55dd0c530e6f4b3db46364f7fb7cd90bbd4f
aelkossei-tech <ayahelkossie@gmail.com>
2025-05-10 13:25:30 -0500

    added Custom Commands notes and practice

---
+++ cypress/e2e/integration/16-customCommands.cy.js
@@ -0,0 +1,59 @@
+/// <reference types="cypress"/>
+
+describe("Cypress Custom Commands", () => {
+    beforeEach(() => {
+      cy.visit("https://www.techglobal-training.com/frontend");
+      cy.clickCard('HTML Elements'); 
+    });
+
+/*
+function clickSearchButton() {
+  cy.get('.searchButton').click()
+}
+
+function searchUser(userName) {
+  cy.get('.searchBar').type(userName)
+  this.clickSearchButton()
+  cy.get('.row').contains(userName).should('have.text', userName)
+}
+
+function editOrShowUserByName(buttonType ,userName) {
+  cy.get('.row').contains(userName).find(buttonType).click()
+  cy.url().should('eq', '/edit/user/18')
+}
+
+function searchAndEditOrShowUserByName(buttonType ,userName){
+  this.searchUser()
+  this.editOrShowUserByName(buttonType ,userName)
+}
+*/
+    it('Parent Commannd', () => {
+        /*Parent Commands --> called ONLY by cy. */
+        // cy.get()
+        // cy.url()
+        // cy.title()
+        // cy.wrap()
+        // cy.window()
+
+        /*
+        ALWAYS starts like this --> Cypress.Commands.add('methodName', (callbackFun/parameters) => {
+            })
+        */
+
+       // cy.selectDropdown('#company_dropdown1', 'Apple'); 
+
+        cy.loginApp('randomEmail@gmail.com', 'TechGlobal'); 
+    }); 
+
+    it.only('Child Commands', () =>{
+        /* Child Commands */
+        // .should()
+        // .find()
+        // .click()
+        // all the action methods 
+
+        cy.get('#main_heading').logText(); 
+        cy.get('#main_heading').haveText('HTML Elements'); 
+    }); 
+}); 
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,10 +15,35 @@
 Cypress.Commands.add('clickCard', (link) => {
     cy.contains('.card, [class*="projectCard"]', link).click();
 }); 
+
+Cypress.Commands.add('selectDropdown', (locator, option) => {
+    cy.get(locator).select(option); 
+}); 
+
+Cypress.Commands.add('loginApp', (email, name) => {
+    cy.get('input[type="email"]').type(email)
+    cy.get('.mb-3 > input').clear().type(name)
+    cy.get('.mb-3 + button').click(); 
+}); 
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
+
+Cypress.Commands.add('logText', { prevSubject: true }, (subject) => {
+    const text = subject.text()
+    cy.log(text); 
+}); 
+
+Cypress.Commands.add('haveText', { prevSubject: 'element' } , (subject, expectedText) => {
+    cy.wrap(subject).should('have.text', expectedText);  
+}); 
+
+Cypress.Commands.add('haveAttrAndValue', { prevSubject: 'element' }, (subject, attribute, value) => {
+    if(value === null) cy.wrap(subject).should('have.attr', attribute)
+    else cy.wrap(attribute).should('have.attr', attribute).and('have.value', value);
+}); 
 //
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
